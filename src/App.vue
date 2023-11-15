0a99ab4e1f029e5c47966a3d6b9e8d9f22491d9b
Yuda Aradea <yudaaradeagit@gmail.com>
2023-11-15 09:16:54 +0700

    Login Register

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,13 @@ import { RouterLink, RouterView } from "vue-router";
         <img src="./assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
       </RouterLink>
       <div class="md:order-2">
-        <a
-          href="login.html"
-          class="px-8 py-3 mt-2 mr-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-sm md:px-8 hover:shadow"
-        >
-          Sign In
-        </a>
-        <a
-          href="register.html"
-          class="px-8 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow"
-        >
-          Sign Up
-        </a>
+        <a href="login.html" class="px-6 py-3 mt-2 mr-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-sm md:px-8 hover:shadow"> Sign In </a>
+        <a href="register.html" class="px-6 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow"> Sign Up </a>
       </div>
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
         <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
           <li>
-            <RouterLink
-              to="/"
-              class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white"
-              aria-current="page"
-              >Home
-            </RouterLink>
+            <RouterLink to="/" class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white" aria-current="page">Home </RouterLink>
           </li>
           <li>
             <RouterLink
