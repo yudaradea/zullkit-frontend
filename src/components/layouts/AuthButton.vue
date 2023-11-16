b026ea75ff6ef74886d819f11a0c498ded10866a
Yuda Aradea <yudaaradeagit@gmail.com>
2023-11-17 01:36:01 +0700

    Login on Mobile

--- src/components/layouts/AuthButton.vue
+++ src/components/layouts/AuthButton.vue
@@ -18,7 +18,7 @@ import { RouterLink } from "vue-router";
     </RouterLink>
 
     <RouterLink
-      to="login"
+      to="/login"
       class="px-8 py-3 text-base font-medium text-white border border-transparent rounded-full bg-navy hover:bg-navy md:py-2 md:text-sm md:px-8 hover:shadow md:hidden"
     >
       Sign In
